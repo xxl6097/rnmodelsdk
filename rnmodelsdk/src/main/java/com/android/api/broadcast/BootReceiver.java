5680ba2aa2d0040c41fcef798876d00fa35dd6bb
Your Name <you@example.com>
2021-12-22 17:14:34 +0800

    fwe

---
+++ rnmodelsdk/src/main/java/com/android/api/broadcast/BootReceiver.java
@@ -0,0 +1,31 @@
+package com.android.api.broadcast;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import com.android.api.util.Logc;
+
+public class BootReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
+            try {
+                Logc.e("自启动了 ！！！！！");
+                Class<?> clasz = Class.forName("com.awesomeproject.MainActivity");
+                Intent newIntent = new Intent(context, clasz);  // 要启动的Activity
+                //1.如果自启动APP，参数为需要自动启动的应用包名
+                //Intent intent = getPackageManager().getLaunchIntentForPackage(packageName);
+                //这句话必须加上才能开机自动运行app的界面
+                newIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                //2.如果自启动Activity
+                context.startActivity(newIntent);
+                //3.如果自启动服务
+                //context.startService(newIntent);
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+}
